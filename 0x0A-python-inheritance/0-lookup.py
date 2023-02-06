d2862044cf79c1cee5fbc0c84eb9b8014be30517
osmangold <osmanahmeddahab@gmail.com>
2023-02-06 11:37:40 -0800

    is good

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+''' function that returns the list
+'''
+
+
 def lookup(obj):
-    return(dir(obj))
+    ''' function: lookup()
+    Returns a list object
+    '''
+    return dir(obj)
