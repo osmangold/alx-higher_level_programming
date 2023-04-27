51b62286d73b461434e671e7af80b7dafb63dff9
osmangold <osmanahmeddahab@gmail.com>
2023-04-27 11:45:01 -0700

    edited

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+
+url=$1
+
+response=$(curl -s -w "%{http_code} %{content_type}\n" "$url")
+
+status_code=$(echo "$response" | awk '{print $1}')
+content_type=$(echo "$response" | awk '{print $2}')
+
+if [[ $status_code -eq 200 && $content_type == "text/html"* ]]; then
+  echo "$response" | sed '1d' | cat
