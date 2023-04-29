983e6e675723ab1f871616da3a9313b2774e5e77
osmangold <osmanahmeddahab@gmail.com>
2023-04-29 13:29:45 -0700

    done

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
 """
-Python script that takes in a letter and sends a POST request
-to http://0.0.0.0:5000/search_user with the letter as a parameter
-"""
-import requests
 import sys
+import requests
 
-if __name__ == '__main__':
 
-    if len(sys.argv) > 1:
-        data = {'q': sys.argv[1]}
-    else:
-        data = {'q': ""}
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
-    r = requests.post('http://0.0.0.0:5000/search_user', data)
-
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        json = r.json()
-        if json == {}:
+        response = r.json()
+        if response == {}:
             print("No result")
         else:
-            print("[{}] {}".format(json['id'], json['name']))
-
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
